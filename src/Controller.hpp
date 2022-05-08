ad021d87a675816ff5d66bfe0c7da56d7a7ee4c6
riaosan <fiatpanda750@icloud.com>
2022-05-08 10:27:45 +0900

    fix ticker

--- src/Controller.hpp
+++ src/Controller.hpp
@@ -40,7 +40,7 @@ using ATOM = ATOMView;
 #endif
 
 class Controller {
-public:
+ public:
   Controller() {}
 
   static void sendMessage(MESSAGE message) {
@@ -52,6 +52,10 @@ public:
   }
 
   static void updatePeriod(void) {
+    sendMessage(MESSAGE::MSG_UPDATE_DOCUMENT);
+  }
+
+  static void firstUpdate(void) {
     sendMessage(MESSAGE::MSG_UPDATE_DOCUMENT);
   }
 
@@ -87,20 +91,18 @@ public:
 
     _atom.begin(SECRET_SSID, SECRET_PASS);
 
-    configTzTime(TIME_ZONE, NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
-
 #if defined(ATOM_DOC)
     _atom.startDocAPI();
     _atom.setAreaCode(27000);
     _serverChecker.attach(60, updatePeriod);
+    _firstChecker.once(15, firstUpdate);
 #else
     _serverChecker.attach(120, updatePeriod);
+    _firstChecker.once(30, firstUpdate);
 #endif
 
+    configTzTime(TIME_ZONE, NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
     _ntpClocker.attach_ms(500, setNtpTime);
-
-    _firstChecker.once(30, updatePeriod);
-
     log_d("Free Heap : %d", heap_caps_get_free_size(MALLOC_CAP_INTERNAL));
   }
 
@@ -129,7 +131,7 @@ public:
   static MESSAGE _message;
   static bool    _clock;
 
-private:
+ private:
   Ticker _serverChecker;
   Ticker _firstChecker;
   Ticker _ntpClocker;
